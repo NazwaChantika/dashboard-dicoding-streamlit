8b698228e134f7458bd47bd25052d43c2611408e
NazwaChantika <127363160+NazwaChantika@users.noreply.github.com>
2024-03-08 12:37:32 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -75,27 +75,6 @@ jam_df = pd.read_csv("https://raw.githubusercontent.com/NazwaChantika/dashboard-
 hari_df = pd.read_csv("https://raw.githubusercontent.com/NazwaChantika/dashboard-dicoding-streamlit/main/Dashboard/hari.csv")
 
 
-# Function to plot bike rental count by weather situation
-def plot_bike_rental_by_weather(df, title):
-    musim_df = df.groupby(by="weathersit").cnt.nunique().reset_index()
-    musim_df.rename(columns={
-        "cnt": "count_sewa"
-    }, inplace=True)
-
-    colors = ["#FFA500", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-    fig, ax = plt.subplots(figsize=(10,5))
-    sb.barplot(
-        y="count_sewa",
-        x="weathersit",
-        data=musim_df.sort_values(by="count_sewa", ascending=False),
-        palette=colors,
-        ax=ax
-    )
-    ax.set_title(title, loc="center", fontsize=15)
-    ax.set_xlabel("Musim")
-    ax.set_ylabel("Banyak Sepeda disewa")
-    st.pyplot(fig)
-
 # Function to display statistics of bike usage by weekday and holiday
 def display_bike_usage_statistics(df, title):
     statistik_penggunaan = df.groupby(['weekday', 'holiday']).agg({
